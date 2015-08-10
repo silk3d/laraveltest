112991dbecb04c390982957496194f8a814c0bb0
Clint Eldridge <celdridge@Clint-Eldridge-SF-15RMBP-Late.local>
2015-08-10 09:00:06 -0700

    add isi php file

--- Opsumit_Site/resources/views/shared/isi.php
+++ Opsumit_Site/resources/views/shared/isi.php
@@ -1,7 +1,77 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: celdridge
- * Date: 7/24/15
- * Time: 1:30 PM
- */
+<h1>Important Safety Information</h1>
+
+
+<h2>What is OPSUMIT?</h2>
+
+OPSUMIT is a prescription medicine used to treat pulmonary arterial hypertension (PAH, WHO Group 1). PAH is high blood pressure in the arteries of your lungs. OPSUMIT can:
+Improve your ability to exercise as measured by the 6-minute walk distance (6MWD). In a clinical study of mainly WHO FC II-III patients, those taking OPSUMIT walked, on average, 22 meters farther at Month 6 than patients not taking it
+Improve some of your symptoms
+Help slow down the progression of your disease. Disease progression included the need for injectable PAH medication or other worsening of PAH (decreased 6MWD, PAH symptoms getting worse, and the need for new PAH treatment)
+Lower your chance of being hospitalized for PAH
+It is not known if OPSUMIT is safe and effective in children.
+What is the most important information about OPSUMIT® (macitentan)?
+
+
+OPSUMIT can cause serious birth defects if taken during pregnancy.
+Females must not be pregnant when starting OPSUMIT or get pregnant while taking OPSUMIT
+Females who are able to get pregnant must have a negative pregnancy test before starting OPSUMIT, each month while taking OPSUMIT, and for 1 month after stopping OPSUMIT. Talk to your healthcare provider about your menstrual cycle. Your healthcare provider will decide when to do the pregnancy tests
+Females who are able to get pregnant are females who have entered puberty, even if they have not started their period, and have a uterus, and have not gone through menopause (defined as not having had a period for at least 12 months for natural reasons, or having had ovaries removed)
+Females who are not able to get pregnant are females who have not yet entered puberty, or do not have a uterus, or have gone through menopause (defined as not having had a period for at least 12 months for natural reasons, or having had ovaries removed)
+Females who are able to get pregnant must use 2 acceptable forms of birth control during treatment with OPSUMIT and for 1 month after stopping OPSUMIT because the medicine may still be in the body. Females who have had a tubal sterilization, a progesterone implant, or have an IUD (intrauterine device) can use these methods alone without any other form of birth control.
+Talk with your healthcare provider to find out about options for acceptable birth control that you may use to prevent pregnancy during treatment with OPSUMIT.
+See the OPSUMIT Medication Guide for acceptable birth control options
+Do not have unprotected sex. Talk to your healthcare provider or pharmacist right away if you have unprotected sex or if you think your birth control has failed. Your healthcare provider may talk with you about using emergency birth control
+Tell your healthcare provider right away if you miss a menstrual period or think you may be pregnant for any reason
+
+If you are the parent or caregiver of a female child who started taking OPSUMIT before reaching puberty, you should check your child regularly to see if she is developing signs of puberty. Your child may reach puberty before having her first menstrual period. Talk to your healthcare provider if you think your child is developing signs of puberty or if you have any questions about the signs of puberty.
+
+
+Females can only receive OPSUMIT through a restricted program called the OPSUMIT Risk Evaluation and Mitigation Strategy (REMS) Program. If you are a female who can get pregnant, you must talk to your healthcare provider, understand the benefits and risks of OPSUMIT, and agree to all of the instructions in the OPSUMIT REMS Program.
+
+
+Males can receive OPSUMIT without taking part in the OPSUMIT REMS Program.
+
+<strong>Who should not take OPSUMIT?</strong>
+
+
+Do not take OPSUMIT if you are pregnant, plan to become pregnant, or become pregnant during treatment with OPSUMIT.
+OPSUMIT can cause serious birth defects. See "What is the most important information about OPSUMIT?"
+
+
+Tell your healthcare provider about all your medical conditions and all the medicines you take, including prescription and nonprescription medicines, vitamins, and herbal supplements. OPSUMIT and other medicines may affect each other causing side effects. Do not start any new medicine until you check with your healthcare provider. Especially tell your healthcare provider if you take an HIV medicine.
+
+What should I avoid while taking OPSUMIT?
+
+
+Do not get pregnant. OPSUMIT can cause serious birth defects. See "What is the most important information about OPSUMIT?"
+If you miss a menstrual period, or think you might be pregnant, call your healthcare provider right away
+You should not breast-feed if you take OPSUMIT. It is not known if OPSUMIT passes into breast milk. Talk to your healthcare provider about the best way to feed your baby if you take OPSUMIT
+
+What are the possible side effects of OPSUMIT?
+
+
+<strong>OPSUMIT can cause serious side effects, including:</strong>
+
+
+Serious birth defects. See “What is the most important information about OPSUMIT?”
+Some medicines that are like OPSUMIT can cause liver problems. Your healthcare provider should do blood tests to check your liver before you start taking OPSUMIT. Tell your healthcare provider if you have any of the following symptoms of liver problems while taking OPSUMIT:
+− Nausea or vomiting
+− Pain in the upper right stomach
+− Tiredness
+− Loss of appetite
+− Your skin or the whites of your eyes turn yellow
+− Dark urine
+− Fever
+− Itching
+Low red blood cell levels (anemia) can happen with OPSUMIT treatment, usually during the first weeks after starting therapy with OPSUMIT. In some cases a blood transfusion may be needed, but this is not common. Your healthcare provider will do blood tests to check your red blood cells before you start OPSUMIT and may also need to do these tests while you are taking OPSUMIT
+Sperm count reduction. Reduced sperm counts have been observed in some men taking a medicine similar to OPSUMIT, an effect which might impair their ability to father a child. Tell your healthcare provider if remaining fertile is important to you
+
+The most common side effects of OPSUMIT are:
+
+Stuffy nose or sore throat, irritation of the airways (bronchitis), headache, flu, and urinary tract infection
+
+
+Tell your healthcare provider about any side effect that bothers you or does not go away. These are not all the possible side effects of OPSUMIT. For more information, ask your healthcare provider or pharmacist.
+Call your healthcare provider for medical advice about side effects. You may report side effects to FDA at 1-800-FDA-1088 or www.fda.gov/medwatch.
+
+Please see full Prescribing Information and Medication Guide, including an Important Warning about Serious Birth Defects.
